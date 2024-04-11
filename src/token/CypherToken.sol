69697f1d6b9e345fad67bb9684c7b4344e2c4baa
DaviDemarqui <davicoding@protonmail.com>
2024-04-11 17:29:04 -0300

    Creation CypherToken

---
+++ src/token/CypherToken.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CypherToken is ERC20 {
+    constructor(uint256 _initialSuppy) ERC20("Cypher", "CYP") {
+        _mint(msg.sender, _initialSuppy);
+    }
+}
