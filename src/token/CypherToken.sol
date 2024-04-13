a0fd378598be52c24f4002db5b2be70845775287
Dave <davicoding@protonmail.com>
2024-04-13 15:25:02 -0300

    Changing token name

--- src/token/CypherToken.sol
+++ src/token/CypherToken.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract CypherToken is ERC20 {
-    constructor(uint256 _initialSuppy) ERC20("Cypher", "CYP") {
+    constructor(uint256 _initialSuppy) ERC20("CYPHER Token", "CYP") {
         _mint(msg.sender, _initialSuppy);
     }
 }
