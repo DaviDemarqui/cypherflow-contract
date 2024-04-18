47bd1294b503770fbfae1bd020723a7483cd0c85
DaviDemarqui <davicoding@protonmail.com>
2024-04-18 16:42:08 -0300

    Minor changes in the Proposal struct

--- src/types/Proposal.sol
+++ src/types/Proposal.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts/utils/structs/EnumerableSet.sol";
 
+// @author: Dave
+// @notice: The proposalType is used to inform what kind of action
+// this proposal will do and also is used to inform the completion
+// function what to do next
 enum ProposaType {
     VoteAnswerWinner,
     DeleteQuestion,
@@ -11,11 +15,13 @@ enum ProposaType {
     UpdateFeeRate
 }
 
+// @notice: "value" is used for updates and also 
+// to pass a information to a function
 struct Proposal {
     uint256 id;
     address proposer;
     string description;
-    uint256 newValue; // Used when updating a value
+    uint256 value; 
     uint256 startTime;
     uint256 endTime;
     uint256 yesVote;
