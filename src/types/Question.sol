2cb768353429d7064f3c312b3afab256d7f16832
DaviDemarqui <davicoding@protonmail.com>
2024-04-18 17:42:30 -0300

    Question struct change

--- src/types/Question.sol
+++ src/types/Question.sol
@@ -1,10 +1,13 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "./Answer.sol";
+
 struct Question {
     bytes32 id;
     string question;
     address creator;
     uint256 createdOn;
+    Answer[] answers;
     bool resolved;
 } 
