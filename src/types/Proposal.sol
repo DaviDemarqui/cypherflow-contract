1167bcc1e2f8192ef3d74633c3a4cbcd778eaab9
DaviDemarqui <davicoding@protonmail.com>
2024-04-18 06:54:55 -0300

    Proposal objects changes

--- src/types/Proposal.sol
+++ src/types/Proposal.sol
@@ -3,17 +3,25 @@ pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts/utils/structs/EnumerableSet.sol";
 
+enum ProposaType {
+    VoteAnswerWinner,
+    DeleteQuestion,
+    DeleteAnswer,
+    UpdateReward,
+    UpdateFeeRate
+}
+
 struct Proposal {
     uint256 id;
     address proposer;
     string description;
-    uint256 amount;
-    address payable recipient;
+    uint256 newValue; // Used when updating a value
     uint256 startTime;
     uint256 endTime;
     uint256 yesVote;
     uint256 noVotes;
     EnumerableSet.AddressSet voters;
+    ProposalType proposalType;
     bool executed;
 }
 
