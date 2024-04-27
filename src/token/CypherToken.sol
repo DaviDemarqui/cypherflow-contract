b5d00418bbc15807c60cf4c11f87b73a14ff15ba
DaviDemarqui <davicoding@protonmail.com>
2024-04-27 18:52:39 -0300

    Adding creator and totalSupply to the CypherToken

--- src/token/CypherToken.sol
+++ src/token/CypherToken.sol
@@ -4,7 +4,15 @@ pragma solidity ^0.8.0;
 import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract CypherToken is ERC20 {
-    constructor(uint256 _initialSuppy) ERC20("CYPHER Token", "CYP") {
-        _mint(msg.sender, _initialSuppy);
+
+    address creator;
+    uint256 totalSupply;
+
+    constructor(uint256 _initialSupply) ERC20("CYPHER Token", "CYP") {
+        _mint(msg.sender, _initialSupply);
+        totalSupply = _initialSupply;
+        creator = msg.sender;
     }
+
+
 }
