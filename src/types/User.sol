bff3512f92ea04c25b9505d36b9090c4557e5be6
DaviDemarqui <davicoding@protonmail.com>
2024-04-28 15:50:53 -0300

    updates in the user struct

--- src/types/User.sol
+++ src/types/User.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 struct User {
     string username;
     address userAddress;
-    uint256 cypEarned;
+    uint256 amountStaked;
     int256 reputation;
+    uint256 rewards;
     bool govMember;
 }
